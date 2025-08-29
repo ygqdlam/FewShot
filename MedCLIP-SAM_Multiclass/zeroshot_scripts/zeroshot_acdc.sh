8b354944997827731f1c493f61500bffb8a961aa
ygqdlam <ygqdlam@gmail.com>
2025-08-29 19:16:41 +0800

    Add files via upload

---
+++ MedCLIP-SAM_Multiclass/zeroshot_scripts/zeroshot_acdc.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# custom config
+
+# Enter the path to your dataset
+#--finetuned \
+DATASET=$1
+
+
+python saliency_maps/generate_saliency_maps.py \
+--input-path ${DATASET}/test_images \
+--output-path saliency_map_outputs/${DATASET}/test_masks \
+--model-name BiomedCLIP \
+--json-path saliency_maps/text_prompts/acdc_prompts.json \
+--reproduce \
+--vvar 0.3 \
+--vbeta 2.0 \
+--vlayer 9 \
+--seed 12 \
+--dataset acdc
+
+python postprocessing/postprocess_saliency_maps.py \
+--input-path ${DATASET}/test_images \
+--output-path coarse_outputs/${DATASET}/test_masks \
+--sal-path saliency_map_outputs/${DATASET}/test_masks \
+--postprocess kmeans \
+--filter
+
+
+
+
+python segment-anything/prompt_sam.py \
+--input ${DATASET}/test_images \
+--mask-input coarse_outputs/${DATASET}/test_masks \
+--output sam_outputs/${DATASET}/test_masks \
+--model-type vit_h \
+--checkpoint segment-anything/sam_checkpoints/sam_vit_h_4b8939.pth \
+--prompts boxes \
+--dataset acdc  
+
+
+
+python evaluation/eval_acdc.py \
+--gt_path ${DATASET}/test_masks \
+--seg_path sam_outputs/${DATASET}/test_masks
+
